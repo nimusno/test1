1e0d031caed08bfa3d732d94ca45044dd817b966
수민 오 <osm369258@naver.com>
2021-10-06 16:33:26 +0900

    수업 내용

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,16 @@
-﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include<stdio.h>
 
-#inc
+int main(void)
+{
+	int a, b, hap = 0;
+	a = 10, b = 20;
+	hap = Add(a, b);
+	return 0;
+}
+
+int Add(int num1, int num2)
+{
+	int sum = 0;
+	sum = num1 + num2;
+	return 0;
+}
